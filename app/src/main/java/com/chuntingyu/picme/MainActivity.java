9837916aa484129a2160d76f37b139153871e082
kevinyu0506 <kevinyu05062006@gmail.com>
2018-04-20 18:19:37 +0800

    搞定pemissionDispatcher & glide

--- app/src/main/java/com/chuntingyu/picme/MainActivity.java
+++ app/src/main/java/com/chuntingyu/picme/MainActivity.java
@@ -1,13 +1,27 @@
 package com.chuntingyu.picme;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPerm();
     }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void checkPerm() {
+//        MainActivityPermissionsDispatcher.showPermissionCheck
+    }
+
 }
