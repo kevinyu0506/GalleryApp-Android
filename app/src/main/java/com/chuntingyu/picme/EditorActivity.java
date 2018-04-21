418cf24dfbc1b26c7dcda073878dbe48a6d298dc
kevinyu0506 <kevinyu05062006@gmail.com>
2018-04-21 21:48:09 +0800

    uCrop匯入完成

--- app/src/main/java/com/chuntingyu/picme/EditorActivity.java
+++ app/src/main/java/com/chuntingyu/picme/EditorActivity.java
@@ -1,12 +1,14 @@
 package com.chuntingyu.picme;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.yalantis.ucrop.UCrop;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -28,6 +30,20 @@ public class EditorActivity extends AppCompatActivity {
         index = getIntent().getIntExtra("value", 0);
 
         Glide.with(this).load(uri).into(img);
+
+        UCrop.of(uri, uri)
+                .withAspectRatio(16, 9)
+                .withMaxResultSize(350, 350)
+                .start(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
     }
 
 
